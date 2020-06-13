03c9c0a475c2a65e9365dc2242ff87643548e660
jorgemagic <jorgemagic@gmail.com>
2020-06-14 00:35:06 +0200

    - Added Cosine and Sampling Light mix project

---
+++ 19-CornellBoxMixtureDensity/PDFs/PDF.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace _19_CornellBoxMixtureDensity
+{
+    public interface PDF
+    {
+        public float Value(Vector3 direction);
+        public Vector3 Generate();
+    }
+}
